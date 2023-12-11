eb978225267c30e8e193704901015fa0885f18af
Gabriel Nascimento <gabrielnascimento2045@gmail.com>
2023-12-11 19:12:13 -0300

    Adicionando Serviço Weather e Interface

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { WeatherService } from './../../services/weather.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: []
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit {
+  initialCityName = 'São Paulo'
+  weatherDatas!:any;
 
+  constructor(private weatherService: WeatherService){}
+
+  ngOnInit(): void {
+    this.getWeatherDatas(this.initialCityName);
+  }
+
+  getWeatherDatas(cityName: string): void{
+    this.weatherService.getWeatherDatas(cityName).subscribe({
+      next: (response)=> {
+        console.log(response)
+      },
+      error:(error) => console.log(error),
+    });
+  }
 }
